a4a67794e720d78b3f8c6af87900bbb32fa2daa9
Ludovic Minvielle <ludominvielle@gmail.com>
2018-03-13 11:41:39 +0100

    autpep8 of transftree & gitignore update

--- transfTree.py
+++ transfTree.py
@@ -241,15 +241,16 @@ def SER(dTree, X_target, y_target):
     print(str(dTree.tree_.node_count) + ' noeuds apres reduction')
     return dTree
 
+
 def SER_RF(random_forest, X_target, y_target):
-  rf_ser = copy.deepcopy(random_forest)
-  for i,dtree in enumerate(rf_ser.estimators_):
-    rf_ser.estimators_[i] = SER(rf_ser.estimators_[i], X_target, y_target)  
-  return rf_ser
+    rf_ser = copy.deepcopy(random_forest)
+    for i, dtree in enumerate(rf_ser.estimators_):
+        rf_ser.estimators_[i] = SER(rf_ser.estimators_[i], X_target, y_target)
+    return rf_ser
 
 if __name__ == "__main__":
     iris = datasets.load_iris()
-    X = iris.data  
+    X = iris.data
     y = iris.target
 
     K = 2
@@ -257,9 +258,9 @@ if __name__ == "__main__":
 
     #rf_s = list()
     #ind_test = list()
-    #kf = StratifiedKFold(n_splits=K) 
-    #for train, test in kf.split(X,y):  
-    #    c = c+1      
+    #kf = StratifiedKFold(n_splits=K)
+    # for train, test in kf.split(X,y):
+    #    c = c+1
     #    #print(c)
     #    rf_s.append(skl_ens.RandomForestClassifier(n_estimators=50,max_depth=10, oob_score=True))
     #    rf_s[-1].fit(X[train],y[train])
@@ -269,20 +270,22 @@ if __name__ == "__main__":
     X_target = X
     y_target = y
 
-    train = np.linspace(0,y.size-1,y.size).astype(int)[::20]
-    all_ = np.linspace(0,y.size-1,y.size).astype(int)
+    train = np.linspace(0, y.size - 1, y.size).astype(int)[::20]
+    all_ = np.linspace(0, y.size - 1, y.size).astype(int)
     test = list(set(all_) - set(train))
 
-    rf = (skl_ens.RandomForestClassifier(n_estimators=50,max_depth=10, oob_score=True))
-    rf.fit(X[train],y[train])
+    rf = (skl_ens.RandomForestClassifier(
+        n_estimators=50, max_depth=10, oob_score=True))
+    rf.fit(X[train], y[train])
     dTree = rf.estimators_[0]
 
-    print('Arbre initial appr. sur '+str(train.size)+' données : '+str(dTree.tree_.node_count)+' noeuds')
-    SER(dTree,X[test],y[test])
+    print('Arbre initial appr. sur ' + str(train.size) +
+          ' données : ' + str(dTree.tree_.node_count) + ' noeuds')
+    SER(dTree, X[test], y[test])
 
-    #==============================================================================
+    #=========================================================================
     #       DEBUG TESTS
-    #==============================================================================
+    #=========================================================================
 
     #t_copy = copy.deepcopy(dTree.tree_)
     #
@@ -292,12 +295,12 @@ if __name__ == "__main__":
     #
     #leaves = np.where(t_copy.feature == -2)[0]
     #
-    ##expansion
-    #print('Expansion...')
+    # expansion
+    # print('Expansion...')
     #stop = 0
-    #for f in leaves :
+    # for f in leaves :
     #    ind = np.where(sparseM.toarray()[:,f] == 1 )[0]
-    #    
+    #
     #    if ind.size != 0 :
     #        Sv = X_target[ind]
     #        #build full new tree from f
@@ -309,23 +312,23 @@ if __name__ == "__main__":
     #t_copy = copy.deepcopy(dTree.tree_)
     #target_values = np.zeros((t_copy.node_count,1,t_copy.value.shape[-1]))
     #sparseM = dTree.decision_path(X_target)
-    #for iy in range(target_values.shape[-1]):
+    # for iy in range(target_values.shape[-1]):
     #    target_values[:,0,iy] = np.sum(sparseM.toarray()[np.where(y_target == iy)[0]],axis = 0)
-    #   
-    #updateValues(dTree.tree_,target_values)
+    #
+    # updateValues(dTree.tree_,target_values)
     #print(str(dTree.tree_.node_count)+' noeuds apres expansion')
     #
-    ## TEST PREDICT CASSE
-    #try:
+    # TEST PREDICT CASSE
+    # try:
     #    dTree.predict(X)
     #    print('PREDICT CONSERVE')
-    #except:
+    # except:
     #    print('PREDICT CASSE ')
     #
-    ###reduction
-    #print('Reduction...')
+    # reduction
+    # print('Reduction...')
     #node_to_cut = list()
-    #for i_node in range(dTree.tree_.node_count):
+    # for i_node in range(dTree.tree_.node_count):
     #    if ( dTree.tree_.feature[i_node] != -2 ):
     #        le = leaf_error(dTree.tree_,i_node )
     #        e,nn= error(dTree.tree_,i_node)
@@ -334,19 +337,17 @@ if __name__ == "__main__":
     #            node_to_cut.append(i_node)
     #
     #print('Indices à couper : ',node_to_cut)
-    #cut_into_leaves(dTree,node_to_cut)
+    # cut_into_leaves(dTree,node_to_cut)
     #
-    ## TEST PREDICT CASSE
-    #try:
+    # TEST PREDICT CASSE
+    # try:
     #    dTree.predict(X)
     #    print('PREDICT CONSERVE')
-    #except:
+    # except:
     #    print('PREDICT CASSE ')
-    #==============================================================================
-    # 
-    #==============================================================================
+    #=========================================================================
+    #
+    #=========================================================================
 
-
-
-print('Arbre initial réajuté sur ' + str(all_.size) +
-      ' données : ' + str(dTree.tree_.node_count) + ' noeuds')
+    print('Arbre initial réajuté sur ' + str(all_.size) +
+          ' données : ' + str(dTree.tree_.node_count) + ' noeuds')
