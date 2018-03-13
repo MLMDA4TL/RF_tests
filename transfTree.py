f5ead189a4b49abadc3fb3043b1835e86d58277e
Mounir <atiq@cmla.ens-cachan.fr>
2018-03-13 13:56:00 +0100

    debut fonction générale pour trouver le meilleur split selon un critère, parmi un ensemble de features donné

--- transfTree.py
+++ transfTree.py
@@ -21,7 +21,7 @@ import pickle
 
 from sklearn import datasets
 import copy
-
+import STRUT as strut
 #==============================================================================
 # 
 #==============================================================================
@@ -97,10 +97,13 @@ def fusionDecisionTree(dTree1, f, dTree2):
   
     
 def leaf_error(tree,node):
-    if node == -1 :
+#    if node == -1 :
+#        return 0
+#    else:
+    if np.sum(tree.value[node]) == 0 :
         return 0
     else:
-        return np.min(tree.value[node])/np.sum(tree.value[node])
+        return 1 - np.max(tree.value[node])/np.sum(tree.value[node])
 
 def error(tree,node):
     if node == -1 : 
@@ -127,12 +130,14 @@ def cut_into_leaves_tree(tree,nodes):
     node_to_rem = list()
     nodes = list(set(nodes))
     
+    size_init = tree.node_count
+    
     for node in nodes:
         
-        size_init = tree.node_count
         node_to_rem = node_to_rem + sub_nodes(tree,node)[1:]
         
     node_to_rem = list(set(node_to_rem))
+    
     depths = depth_array(tree,list(set(np.linspace(0,size_init-1,size_init).astype(int))-set(node_to_rem)))
     dic['max_depth'] = np.max(depths)
     
@@ -220,6 +225,72 @@ def SER_RF(random_forest, X_target, y_target):
     rf_ser.estimators_[i] = SER(rf_ser.estimators_[i], X_target, y_target)  
   return rf_ser
 
+def all_splits(X,ind_feats,min_delta = 10e-7):
+    x_sorted = X[:,ind_feats].copy()
+    th = np.zeros(ind_feats.size,dtype = object)
+    inds = np.zeros(ind_feats.size,dtype = object) 
+    
+    for c in range(x_sorted.shape[1]):
+        x_sorted[:,c] = np.sort(x_sorted[:,c])
+        
+        var = x_sorted[1:,c]- x_sorted[:-1,c]
+        inds[c] = np.where(var > min_delta*(max(x_sorted[:,c]) - min(x_sorted[:,c])))[0]  
+        th[c] = (x_sorted[inds[c]+1,c]+x_sorted[inds[c],c] ) /2
+        
+    return th
+
+def search_best_split(decision_tree,node_index, ind_feats, Q_source_parent,
+                        X_target_node,
+                        Y_target_node,
+                        classes, measure_func = [[strut.DG,strut.IG]]):
+    
+    ths = all_splits(X,ind_feats,min_delta = 10e-7)
+
+    #Q_source_l,Q_source_r = get_children_distributions(decisiontree,node_index)
+    #Q_source_parent = get_node_distribution(decisiontree,node_index)
+        
+    bool_ = 0
+    f_opt = -1
+    #ind_th_opt = -1
+    
+    ind_f = -1
+    #ind_th = -1
+    
+    and_block_score = np.zeros(len(measure_func),dtype = object)
+    and_block_score_opt = np.zeros(len(measure_func),dtype = object)
+    for i,and_block in enumerate(measure_func):
+        and_block_score[i] = np.zeros(len(and_block))
+        and_block_score_opt[i] = np.zeros(len(and_block))
+        
+    for f in ths:
+        ind_f =  ind_f + 1
+        for th in f:
+            #ind_th = ind_th + 1
+            
+            
+            Q_target_l,Q_target_r = strut.compute_Q_children_target(X_target_node,
+                                                              Y_target_node,
+                                                              f,
+                                                              th,
+                                                              classes)
+            for i,and_block in enumerate(measure_func):
+                bool_and = 1
+                for j,func in enumerate(and_block) :
+                    and_block_score[i][j] = func( Q_source_parent,[Q_target_l,Q_target_r])
+                    if and_block_score[i][j] <= and_block_score_opt[i][j] :
+                        bool_and = 0
+                    else:
+                        and_block_score_opt[i][j] = and_block_score[i][j]
+                        
+                bool_ = bool_ + bool_and
+                
+            if (bool_):
+                f_opt = ind_f
+                #ind_th_opt = ind_th
+                th_opt = th
+    
+    return  ind_feats[f_opt], th_opt
+
 if __name__ == "__main__":
     iris = datasets.load_iris()
     X = iris.data  
@@ -228,17 +299,6 @@ if __name__ == "__main__":
     K = 2
     c = 0
 
-    #rf_s = list()
-    #ind_test = list()
-    #kf = StratifiedKFold(n_splits=K) 
-    #for train, test in kf.split(X,y):  
-    #    c = c+1      
-    #    #print(c)
-    #    rf_s.append(skl_ens.RandomForestClassifier(n_estimators=50,max_depth=10, oob_score=True))
-    #    rf_s[-1].fit(X[train],y[train])
-    #    ind_test.append(test)
-    #
-    #dTree = rf_s[0].estimators_[0]
     X_target = X
     y_target = y
 
@@ -252,72 +312,6 @@ if __name__ == "__main__":
 
     print('Arbre initial appr. sur '+str(train.size)+' données : '+str(dTree.tree_.node_count)+' noeuds')
     SER(dTree,X[test],y[test])
-
-    #==============================================================================
-    #       DEBUG TESTS
-    #==============================================================================
-
-    #t_copy = copy.deepcopy(dTree.tree_)
-    #
-    #sparseM = dTree.decision_path(X_target)
-    ##target_values[:,:,0] = np.sum(sparseM.toarray()[np.where(y_target == 0)[0]],axis = 0)
-    ##target_values[:,:,1] = np.sum(sparseM.toarray()[np.where(y_target == 1)[0]],axis = 0)
-    #
-    #leaves = np.where(t_copy.feature == -2)[0]
-    #
-    ##expansion
-    #print('Expansion...')
-    #stop = 0
-    #for f in leaves :
-    #    ind = np.where(sparseM.toarray()[:,f] == 1 )[0]
-    #    
-    #    if ind.size != 0 :
-    #        Sv = X_target[ind]
-    #        #build full new tree from f
-    #        DT_to_add = sklearn.tree.DecisionTreeClassifier()
-    #        DT_to_add.min_impurity_split = 0
-    #        DT_to_add.fit(Sv,y_target[ind])
-    #        fusionDecisionTree(dTree, f, DT_to_add)
-    #
-    #t_copy = copy.deepcopy(dTree.tree_)
-    #target_values = np.zeros((t_copy.node_count,1,t_copy.value.shape[-1]))
-    #sparseM = dTree.decision_path(X_target)
-    #for iy in range(target_values.shape[-1]):
-    #    target_values[:,0,iy] = np.sum(sparseM.toarray()[np.where(y_target == iy)[0]],axis = 0)
-    #   
-    #updateValues(dTree.tree_,target_values)
-    #print(str(dTree.tree_.node_count)+' noeuds apres expansion')
-    #
-    ## TEST PREDICT CASSE
-    #try:
-    #    dTree.predict(X)
-    #    print('PREDICT CONSERVE')
-    #except:
-    #    print('PREDICT CASSE ')
-    #
-    ###reduction
-    #print('Reduction...')
-    #node_to_cut = list()
-    #for i_node in range(dTree.tree_.node_count):
-    #    if ( dTree.tree_.feature[i_node] != -2 ):
-    #        le = leaf_error(dTree.tree_,i_node )
-    #        e,nn= error(dTree.tree_,i_node)
-    #        if le <= e :
-    #            print(str(le)+' <= '+str(e))
-    #            node_to_cut.append(i_node)
-    #
-    #print('Indices à couper : ',node_to_cut)
-    #cut_into_leaves(dTree,node_to_cut)
-    #
-    ## TEST PREDICT CASSE
-    #try:
-    #    dTree.predict(X)
-    #    print('PREDICT CONSERVE')
-    #except:
-    #    print('PREDICT CASSE ')
-    #==============================================================================
-    # 
-    #==============================================================================
 
 
     print('Arbre initial réajuté sur '+str(all_.size)+' données : '+str(dTree.tree_.node_count)+' noeuds')
