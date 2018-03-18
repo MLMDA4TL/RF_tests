3f8e3c8711bcc753ed4753ac9f634546d971c44f
Ludovic Minvielle <ludominvielle@gmail.com>
2018-03-18 21:01:32 +0100

    real data loading and testing with SER

--- test_SER.py
+++ test_SER.py
@@ -1,57 +1,79 @@
-import pandas as pd
-import numpy as np
 import sklearn.ensemble as skl_ens
 from sklearn.tree import export_graphviz
 
+import data
 from transfTree import SER
 from STRUT import STRUT
 from transfTree import fusionDecisionTree
 
+# =======================================================
+#   Parameters
+# =======================================================
+NB_TREE = 50
+APPLY_SER = 1
 
-df = pd.read_csv("data/letter/letter-recognition.data.txt",
-                 sep=',', header=None)
-letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-label = {letter: i for i, letter in enumerate(letters)}
-df.iloc[:, 0] = [label[l] for l in df.iloc[:, 0]]
+# =======================================================
+#   Data
+# =======================================================
+X_source, X_target_005, X_target_095, y_source, y_target_005, y_target_095 = data.load_mushroom()
 
-X_source = df[df[9] < np.median(df[9])]
-X_target = df[df[9] >= np.median(df[9])]
-X_target_005 = X_target.iloc[
-    0: int(X_target.shape[0] * 0.05), list(range(1, len(X_source.columns)))]
-y_target_005 = X_target.iloc[0: int(X_target.shape[0] * 0.05), 0]
-X_target_095 = X_target.iloc[
-    int(X_target.shape[0] * 0.05):, list(range(1, len(X_source.columns)))]
-y_target_095 = X_target.iloc[int(X_target.shape[0] * 0.05):, 0]
+# =======================================================
+#   CLF initialisation
+# =======================================================
+rf_source = skl_ens.RandomForestClassifier(
+    n_estimators=NB_TREE, oob_score=True)
+rf_source.fit(X_source, y_source)
 
-rf_source = skl_ens.RandomForestClassifier(n_estimators=50, oob_score=True)
-rf_source.fit(X_source[list(range(1, len(X_source.columns)))], X_source[0])
-rf_source.score(X_target[list(range(1, len(X_source.columns)))], X_target[0])
+rf_source_score_target = rf_source.score(X_target_095, y_target_095)
 
-rf_target = skl_ens.RandomForestClassifier(n_estimators=50, oob_score=True)
+print("Error rate de rf_source sur data target : ",
+      round(1 - rf_source_score_target, 2))
+
+rf_target = skl_ens.RandomForestClassifier(n_estimators=NB_TREE, oob_score=True,
+                                           class_weight='balanced')
 
 rf_target.fit(X_target_005, y_target_005)
+rf_target_score_target = rf_target.score(X_target_095, y_target_095)
 
-rf_target.score(X_target_095, y_target_095)
+print("Error rate de rf_target(5%) sur data target(95%) : ",
+      round(1 - rf_target_score_target, 2))
 
-dtree0 = rf_source.estimators_[0]
-dtree1 = rf_source.estimators_[1]
+# dtree0 = rf_source.estimators_[0]
+# dtree1 = rf_source.estimators_[1]
 
-print("noeuds dtree0 : ", dtree0.tree_.node_count)
-print("noeuds dtree1 : ", dtree1.tree_.node_count)
+# dtree_test = skl_tree.DecisionTreeClassifier()
+# dtree_test.fit(X_target_005, y_target_005)
 
-export_graphviz(dtree0, "output/dtree_0.dot")
-export_graphviz(dtree1, "output/dtree_1.dot")
+# print("noeuds dtree0 : ", dtree0.tree_.node_count)
+# print("noeuds dtree1 : ", dtree1.tree_.node_count)
 
-dtree_SER_0 = SER(dtree0, X_target_005.values, y_target_005.values)
-dtree_SER_1 = SER(dtree1, X_target_005.values, y_target_005.values)
+# export_graphviz(dtree0, "output/dtree_0.dot")
+# export_graphviz(dtree1, "output/dtree_1.dot")
 
-export_graphviz(dtree0, "output/dtree_0_SER.dot")
-export_graphviz(dtree1, "output/dtree_1_SER.dot")
+# dtree_SER_0 = SER(dtree0, X_target_005.values, y_target_005.values)
+# dtree_SER_1 = SER(dtree1, X_target_005.values, y_target_005.values)
 
-dtree_fusion = fusionDecisionTree(dtree0, 1, dtree1)
-export_graphviz(dtree_fusion, "output/dtree_fusion.dot")
+# export_graphviz(dtree0, "output/dtree_0_SER.dot")
+# export_graphviz(dtree1, "output/dtree_1_SER.dot")
 
-dtree_STRUT = STRUT(dtree0, 0, X_target_005.values, y_target_005.values)
+# dtree_fusion = fusionDecisionTree(dtree0, 1, dtree1)
+# export_graphviz(dtree_fusion, "output/dtree_fusion.dot")
 
-# for i, dtree in enumerate(rf_source.estimators_):
-    # print("Tree ", i)
+# dtree_STRUT = STRUT(dtree0, 0, X_target_005.values, y_target_005.values)
+
+# =======================================================
+#   SER algorithm
+# =======================================================
+if APPLY_SER:
+    for i in range(NB_TREE):
+        print("Tree ", i)
+        print("nb noeuds initial : ",
+              rf_source.estimators_[i].tree_.node_count)
+        output = SER(rf_source.estimators_[i], X_target_005, y_target_005)
+        print("nb noeuds après SER : ",
+              rf_source.estimators_[i].tree_.node_count)
+
+    # now every tree of rf_sourse has been modified
+    rf_source_SER_score = rf_source.score(X_target_095, y_target_095)
+    print("Error rate de rf_source_SER sur data target(95%) : ",
+          round(1 - rf_source_SER_score, 2))
