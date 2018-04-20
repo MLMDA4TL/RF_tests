e459795fcdb1e5b2860e5f40085354ce5c0acc49
SergioPeignier <sergio.peignier.zapata@gmail.com>
2018-04-20 17:40:12 +0200

    neural random forests

--- code/nrf/Neural_Decision_Tree.py
+++ code/nrf/Neural_Decision_Tree.py
@@ -11,8 +11,6 @@ import sys
 sys.path.append("../utils")
 from common_functions import find_parent,leaves_id,get_list_split_phi_forest,get_list_split_phi,get_parents_nodes_leaves_dic
 from collections import OrderedDict 
-
-
 from keras.models import Model
 from keras.layers import Input, Dense, Dropout
 from keras.callbacks import ModelCheckpoint,EarlyStopping
@@ -23,12 +21,17 @@ from sklearn.metrics import accuracy_score
 
 BIAS = 1
 DIM = 0
-class dt2nn:
-  def __init__(self,decision_tree,D,gammas=[10,1,1]):
+
+
+class ndt:
+  def __init__(self,D,gammas=[10,1,1],tree_id=None):
     self.gammas = gammas
+    self.D = D
+    self.tree_id = tree_id
+
+  def compute_matrices_and_biases(self, decision_tree):
     self.splits = pd.DataFrame(get_list_split_phi(decision_tree)).T
     self.leaves = get_parents_nodes_leaves_dic(decision_tree)
-    self.D = D
     self.N = self.splits.shape[0]
     self.L = len(self.leaves)
     # Fill Input -> Nodes layer matrix
@@ -38,7 +41,8 @@ class dt2nn:
     for node, dim in self.splits[DIM].iteritems():
       self.W_in_nodes.loc[dim,node] = 1.*self.gammas[0]
     # Fill Input -> Nodes layer biases
-    self.b_nodes = - self.splits[BIAS]*self.gammas[0]
+    self.b_nodes = pd.DataFrame(- self.splits[BIAS]*self.gammas[0])
+    self.b_nodes.columns = ["NODES_BIASES"]
     # Fill Nodes -> Leaves layer matrix
     self.W_nodes_leaves = pd.DataFrame(np.zeros((self.N,self.L)),
                                       index = self.splits.index,
@@ -48,7 +52,9 @@ class dt2nn:
         self.W_nodes_leaves.loc[node,leave] = r_l*self.gammas[1]
     # Fill Nodes -> Leaves layer biases       
     b_leaves = {k:-len(x)+0.5 for k,x in self.leaves.iteritems()}
-    self.b_leaves = pd.DataFrame(b_leaves.values(), b_leaves.keys())
+    self.b_leaves = pd.DataFrame(b_leaves.values(),
+                                 index=b_leaves.keys(),
+                                 columns = ["LEAVES_BIASES"])
     self.b_leaves *= self.gammas[1]
     # Fill Leaves -> class matrix
     self.classes = decision_tree.classes_
@@ -61,13 +67,32 @@ class dt2nn:
     self.W_leaves_out = (self.W_leaves_out.T * 1./ self.W_leaves_out.T.sum()).T
     self.W_leaves_out *= self.gammas[2]
     # Fill class biases
-    self.b_class = pd.DataFrame(np.zeros(self.C), index = self.classes)
+    self.b_class = pd.DataFrame(np.zeros(self.C),
+                                index = self.classes,
+                                columns = ["CLASS_BIASES"])
+    self.specify_tree_id()
 
-
-
+  def specify_tree_id(self):
+    def add_id_2_elements(list_values, id_to_add):
+      append_id = lambda x:str(id_to_add)+"_"+str(x)
+      return map(append_id,list_values)
+    if self.tree_id != None:
+      # Rename input -> nodes matrix biases
+      self.W_in_nodes.columns = add_id_2_elements(self.W_in_nodes.columns,
+                                                  self.tree_id)
+      self.b_nodes.index = add_id_2_elements(self.b_nodes.index,self.tree_id)
+      # Rename nodes -> leaves matrix biases
+      self.W_nodes_leaves.index = add_id_2_elements(self.W_nodes_leaves.index,
+                                                    self.tree_id)
+      self.W_nodes_leaves.columns = add_id_2_elements(self.W_nodes_leaves.columns,
+                                                      self.tree_id)
+      self.b_leaves.index = add_id_2_elements(self.b_leaves.index,self.tree_id)
+      # Rename leaves -> classes matrix biases
+      self.W_leaves_out.index = add_id_2_elements(self.W_leaves_out.index,
+                                                  self.tree_id)
 
   def to_keras(self,
-               dropouts = [0.1,0.1],
+               dropouts = [0.1,0.1,0.1],
                loss='categorical_crossentropy',
                optimizer = optimizers.Adam,
                optimizer_params = {"lr":0.001,
@@ -76,26 +101,32 @@ class dt2nn:
                                    "epsilon":1e-8,
                                    "decay":1e-6}):
     self.input_layer = Input(shape=(self.D,))
-    self.nodes_layer = Dense(self.N,
-                        activation="tanh")(self.input_layer)
+    self.drop_input_layer = Dropout(dropouts[0])(self.input_layer)
 
-    self.drop_nodes_layer = Dropout(dropouts[0])(self.nodes_layer)
+    self.nodes_layer = Dense(self.N,
+                        activation="tanh")(self.drop_input_layer)
+
+    self.drop_nodes_layer = Dropout(dropouts[1])(self.nodes_layer)
+
     self.leaves_layer = Dense(self.L,
                         activation="tanh")(self.drop_nodes_layer)
-    self.drop_nodes_layer = Dropout(dropouts[1])(self.leaves_layer)
+    self.drop_nodes_layer = Dropout(dropouts[2])(self.leaves_layer)
+
     self.output_layer = Dense(self.C, activation='softmax')(self.drop_nodes_layer)
+
     self.model = Model(input=self.input_layer, output=self.output_layer)
     self.model_nodes = Model(input=self.input_layer, output=self.nodes_layer)
     self.model_leaves = Model(input=self.input_layer, output=self.leaves_layer)
 
     self.sgd = optimizer(**optimizer_params)
     self.model.compile(loss=loss, optimizer=self.sgd)
+    print self.model.summary()
 
-    self.model.layers[1].set_weights(weights=[self.W_in_nodes,
+    self.model.layers[2].set_weights(weights=[self.W_in_nodes,
                                              self.b_nodes.values.flatten()])
-    self.model.layers[3].set_weights(weights=[self.W_nodes_leaves,
+    self.model.layers[4].set_weights(weights=[self.W_nodes_leaves,
                                              self.b_leaves.values.flatten()])
-    self.model.layers[5].set_weights(weights=[self.W_leaves_out,
+    self.model.layers[6].set_weights(weights=[self.W_leaves_out,
                                               self.b_class.values.flatten()])
 
   def fit(self,
@@ -103,7 +134,7 @@ class dt2nn:
           y,
           epochs=100,
           min_delta=0,
-          patience=0,
+          patience=10,
           ):
     y = to_categorical(y)
     early_stopping = EarlyStopping(monitor='loss',
@@ -123,35 +154,53 @@ class dt2nn:
               X,
               y = None):
     nodes_a = pd.DataFrame(self.model_nodes.predict(X),
-                           index = self.W_in_nodes.index,
-                           columns = self.W_in_nodes.columns)
+                           columns = self.b_nodes.index)
     leaves_a = pd.DataFrame(self.model_leaves.predict(X),
-                            index = self.W_nodes_leaves.index,
-                            columns = self.W_nodes_leaves.columns)
+                            columns = self.b_leaves.index)
     output_a = pd.DataFrame(self.model.predict(X),
-                            index = self.W_leaves_out.index,
-                            columns = self.W_leaves_out.columns)
+                            columns = self.b_class.index)
     return nodes_a, leaves_a, output_a
 
   def get_weights_from_NN(self):
-    w_1 = self.model.layers[1].get_weights()
+    w_2 = self.model.layers[2].get_weights()
                        
     self.W_in_nodes_nn = pd.DataFrame(w_1[0],
                          index = self.W_in_nodes.index,
                          columns = self.W_in_nodes.columns)
-    self.b_nodes = pd.DataFrame(w_1[1],index=self.b_nodes.index)
+    self.b_nodes_nn = pd.DataFrame(w_1[1],
+                                index=self.b_nodes.index,
+                                columns=self.b_nodes.columns)
 
-    w_3 = self.model.layers[3].get_weights()
+    w_4 = self.model.layers[4].get_weights()
     self.W_nodes_leaves_nn = pd.DataFrame(w_3[0],
                              index = self.W_nodes_leaves.index,
                              columns = self.W_nodes_leaves.columns)
-    self.b_leaves_nn = pd.DataFrame(w_3[1],index=self.b_leaves.index)
+    self.b_leaves_nn = pd.DataFrame(w_3[1],
+                                    index=self.b_leaves.index,
+                                    columns=self.b_leaves.columns)
 
-    w_5 = self.model.layers[5].get_weights()
+    w_6 = self.model.layers[6].get_weights()
     self.W_leaves_out_nn = pd.DataFrame(w_5[0],
-                           index = self.W_leaves_out.index,
-                          columns = self.W_leaves_out.columns)
-    self.b_class_nn = pd.DataFrame(w_5[1],index=self.b_class.index)
+                                        index = self.W_leaves_out.index,
+                                        columns = self.W_leaves_out.columns)
+    self.b_class_nn = pd.DataFrame(w_5[1],
+                                   index=self.b_class.index,
+                                   columns=self.b_class.columns)
+
+  def compute_weights_differences(self):
+    self.get_weights_from_NN()
+    diff_W_in_nodes = self.W_in_nodes - self.W_in_nodes_nn
+    diff_b_nodes = self.b_nodes - self.b_nodes_nn
+    diff_W_nodes_leaves = self.W_nodes_leaves - self.W_nodes_leaves_nn
+    diff_b_leaves = self.b_leaves - self.b_leaves_nn
+    diff_W_leaves_output = self.W_leaves_out - self.W_leaves_out_nn
+    diff_b_class = self.b_class - self.b_class_nn
+    return diff_W_in_nodes,\
+           diff_b_nodes,\
+           diff_W_nodes_leaves,\
+           diff_b_leaves,\
+           diff_W_leaves_output,\
+           diff_b_class
 
   def predict_class(self,
                     X,
@@ -164,10 +213,39 @@ class dt2nn:
             y):
     return accuracy_score(self.predict_class(X),y)
 
+  def print_tree_weights(self):
+    print("W: Input -> Nodes")
+    print(self.W_in_nodes)
+    print("b: Input -> Nodes")
+    print(self.b_nodes)
+    print("W: Nodes -> Leaves")
+    print(self.W_nodes_leaves)
+    print("b: Nodes -> Leaves")
+    print(self.b_leaves)
+    print("W: Leaves -> Out")
+    print(self.W_leaves_out)
+    print("b: Leaves -> Out")
+    print(self.b_class)
+
+  def print_nn_weights(self):
+    print("W: Input -> Nodes")
+    print(self.W_in_nodes_nn)
+    print("b: Input -> Nodes")
+    print(self.b_nodes_nn)
+    print("W: Nodes -> Leaves")
+    print(self.W_nodes_leaves_nn)
+    print("b: Nodes -> Leaves")
+    print(self.b_leaves_nn)
+    print("W: Leaves -> Out")
+    print(self.W_leaves_out_nn)
+    print("b: Leaves -> Out")
+    print(self.b_class_nn)
+
+
+
 if __name__ == "__main__":
-  import graphviz 
-  import matplotlib.pyplot as plt
   # Build a dataset
+  import matplotlib.pyplot as plt
   dataset_length = 1000
   D = 2
   X = np.random.randn(dataset_length,D)*0.1
@@ -175,39 +253,34 @@ if __name__ == "__main__":
   X[0:dataset_length//2,0] += 0.2
   Y = np.ones(dataset_length)
   Y[0:dataset_length//2] *= 0
-  # Train a Tree
-  clf = DecisionTreeClassifier()
-  clf = clf.fit(X, Y)
-
-  # plot the tree
-  dot_data = export_graphviz(clf, out_file=None, 
-                           feature_names=["feature_"+str(i) for i in range(D)],  
-                           class_names=["class_0", "class_1"],  
-                           filled=True, rounded=True,  
-                           special_characters=True)  
-
-  graph = graphviz.Source(dot_data)  
-  graph.render('prarent_tree.gv', view=True) 
-  print(get_list_split_phi(clf))
-
-  rf = RandomForestClassifier(max_depth=5,n_estimators=100)
-  rf.fit(X, Y)
-  forest_splits = get_list_split_phi_forest(rf)
-
-  splits_df = pd.DataFrame(forest_splits)
 
   X_test = np.random.randn(dataset_length,D)*0.1
   X_test[0:dataset_length//2,0] += 0.1
   X_test[0:dataset_length//2,0] += 0.2
   Y_test = np.ones(dataset_length)
   Y_test[0:dataset_length//2] *= 0
+  # Train a Tree
+  clf = DecisionTreeClassifier()
+  clf = clf.fit(X, Y)
 
-  a = dt2nn(clf,2,gammas=[10,1,1])
+  a = ndt(D=2,gammas=[10,1,1],tree_id=0)
+  a.compute_matrices_and_biases(clf)
   a.to_keras()
+  print "scores before training"
   print a.score(X_test,Y_test)
   print a.score(X,Y)
   errors = a.fit(X,Y)
-
+  print "scores after training"
   print a.score(X_test, Y_test)
   print a.score(X,Y)
   a.get_weights_from_NN()
+  print "Tree weights"
+  a.print_tree_weights()
+  print "NN weights"
+  a.print_nn_weights()
+  print "activations"
+  print a.get_activations(X)
+  differences = a.compute_weights_differences()
+  for diff in differences:
+    plt.imshow(diff)
+    plt.show()
---
+++ code/nrf/Neural_Random_Forest.py
@@ -0,0 +1,167 @@
+# -*- coding: utf-8 -*-
+"""
+@author: sergio
+"""
+
+import numpy as np
+from sklearn.tree import DecisionTreeClassifier,export_graphviz
+from sklearn.ensemble import RandomForestClassifier
+import pandas as pd
+import sys
+sys.path.append("../utils")
+from common_functions import find_parent,leaves_id,get_list_split_phi_forest,get_list_split_phi,get_parents_nodes_leaves_dic
+from collections import OrderedDict 
+
+from Neural_Decision_Tree import ndt
+from keras.models import Model
+from keras.layers import Input, Dense, Dropout, Concatenate
+from keras.callbacks import ModelCheckpoint,EarlyStopping
+from keras import optimizers
+from sklearn.preprocessing import LabelEncoder
+from keras.utils import to_categorical
+from sklearn.metrics import accuracy_score
+
+
+
+class nrf_fully_connected(ndt):
+  def __init__(self, D, gammas=[10,1,1,1]):
+    ndt.__init__(self, D, gammas)
+
+  def compute_matrices_and_biases(self, random_forest):
+    self.rf = random_forest
+    self.classes = self.rf.classes_
+    self.ndts = []
+    # Compute weights and biases of individual trees
+    for i,dtree in enumerate(random_forest.estimators_):
+      self.ndts.append(ndt(self.D, self.gammas, i))
+      self.ndts[i].compute_matrices_and_biases(dtree)
+    # Concatenate input to nodes W
+    self.W_in_nodes = pd.concat([t.W_in_nodes for t in self.ndts],axis=1)
+    # Concatenate input to nodes b
+    self.b_nodes = pd.concat([t.b_nodes for t in self.ndts],axis=0)
+    # Concatenate nodes to leaves W
+    self.W_nodes_leaves = pd.concat([t.W_nodes_leaves for t in self.ndts],axis=1)
+    self.W_nodes_leaves = self.W_nodes_leaves.fillna(0)
+    # Concatenate nodes to leaves b
+    self.b_leaves = pd.concat([t.b_leaves for t in self.ndts],axis=0)
+    # Concatenate leaves to out W
+    self.W_leaves_out = pd.concat([t.W_leaves_out for t in self.ndts],axis=0)
+    # Concatenate leaves to out b
+    self.b_class = pd.concat([t.b_class for t in self.ndts],axis=0)
+    self.b_class = self.b_class.groupby(self.b_class.index).mean()
+    # Set other parameters
+    self.N = self.b_nodes.size
+    self.L = self.b_leaves.size
+    self.C = self.b_class.size
+
+class nrf_independent_ndt(ndt):
+  def __init__(self, D, gammas=[10,1,1,1]):
+    ndt.__init__(self, D, gammas)
+
+  def compute_weights_differences(self, random_forest):
+    self.rf = random_forest
+    self.classes = self.rf.classes_
+    self.ndts = []
+    for i,dtree in enumerate(random_forest.estimators_):
+      self.ndts.append(ndt(self.D, self.gammas, i))
+      self.ndts[i].compute_matrices_and_biases(dtree)
+    # Define averaging layer
+    self.W_outputs_to_output = pd.DataFrame(np.concatenate([np.eye(self.C)*self.gammas[-1] for tree in self.ndts], axis=0),
+                                            index=[str(t.tree_id)+"_"+str(c) for t in self.ndts for t_class in t.classes],
+                                            columns=self.classes)
+    self.b_class = pd.DataFrame(np.zeros(self.C),
+                                index = self.classes,
+                                columns = ["CLASS_BIASES"])
+  def to_keras(self):
+    # Compute keras models for other trees
+    for tree in self.ndts:
+      tree.to_keras()
+    # Define the averaging model
+    self.concatenation_output_layers = Concatenate([t.output_layer for t in self.ndts])
+    self.dropouts_output_layers = Dropout(dropouts[2])(self.concatenation_output_layers)
+    self.output_layer = Dense(self.C, activation='softmax')(self.dropouts_output_layers)
+    self.model = Model(input=[t.input_layer for t in self.ndts], output=self.output_layer)
+    self.model.compile(loss=loss, optimizer=self.sgd)
+    self.model.layers[8].set_weights(weights=[W_outputs_to_output,
+                                              self.b_class])
+
+  def get_activations(self,
+                      X,
+                      y=None):
+    output_a = pd.DataFrame(self.model.predict(X),
+                            columns=self.b_class.columns)
+    return [t.get_activations(X,y) for t in self.ndts]+[[output_a]]
+
+  def get_weights_from_NN(self):
+    for tree in self.ndts:
+      tree.get_weights_from_NN()
+    w_9 = self.model.layers[9].get_weights()
+    self.W_outputs_to_output_nn = pd.DataFrame(w_9[0],
+                                               index=[str(t.tree_id)+"_"+str(c) for t in self.ndts for t_class in t.classes],
+                                               columns=self.classes)
+    self.b_class_nn = pd.DataFrame(w_9[1],
+                                   index = self.classes,
+                                   columns = ["CLASS_BIASES"])
+
+  def compute_weights_differences(self):
+    self.get_weights_from_NN()
+    diff_W_outputs_to_output_nn = self.W_outputs_to_output - self.W_outputs_to_output_nn
+    diff_b_class_nn = self.b_class_nn - self.b_class
+    return [t.compute_weights_differences() for t in self.ndts] + [[diff_W_outputs_to_output_nn,diff_b_class_nn]]
+
+  def print_tree_weights(self):
+    for t in self.ndts:
+      t.print_tree_weights()
+    print("W: outputs -> output")
+    print(self.W_outputs_to_output)
+    print("b: output")
+    print(self.b_class)
+
+  def print_nn_weights(self):
+    for t in self.ndts:
+      t.print_nn_weights()
+    print("W: outputs -> output")
+    print(self.W_outputs_to_output_nn)
+    print("b: output")
+    print(self.b_class_nn)    
+
+if __name__ == "__main__":
+  import matplotlib.pyplot as plt
+  dataset_length = 1000
+  D = 2
+  X = np.random.randn(dataset_length,D)*0.1
+  X[0:dataset_length//2,0] += 0.1
+  X[0:dataset_length//2,0] += 0.2
+  Y = np.ones(dataset_length)
+  Y[0:dataset_length//2] *= 0
+
+  X_test = np.random.randn(dataset_length,D)*0.1
+  X_test[0:dataset_length//2,0] += 0.1
+  X_test[0:dataset_length//2,0] += 0.2
+  Y_test = np.ones(dataset_length)
+  Y_test[0:dataset_length//2] *= 0
+  # Train a Tree
+  rf = RandomForestClassifier(max_depth=5,n_estimators=10)
+  rf.fit(X, Y)
+
+  a = nrf_fully_connected(D=2,gammas=[10,1,1])
+  a.compute_matrices_and_biases(rf)
+  a.to_keras()
+  print "scores before training"
+  print a.score(X_test,Y_test)
+  print a.score(X,Y)
+  errors = a.fit(X,Y)
+  print "scores after training"
+  print a.score(X_test, Y_test)
+  print a.score(X,Y)
+  a.get_weights_from_NN()
+  print "Tree weights"
+  a.print_tree_weights()
+  print "NN weights"
+  a.print_nn_weights()
+  print "activations"
+  print a.get_activations(X)
+  differences = a.compute_weights_differences()
+  for diff in differences:
+    plt.imshow(diff)
+    plt.show()
